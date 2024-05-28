d2ba3bc35b634128e370784ee941dd0069ff7629
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-05-28 21:40:51 +0700

    menambahkan servo

--- smart_mini_conveyor.ino
+++ smart_mini_conveyor.ino
@@ -19,9 +19,21 @@ const int echoPin = 32;
 const int trigPin = 33;
 float duration,distance;
 
+// variabel counting
+int itemA = 0;
+int itemB = 0;
+
+bool itemDetectedA = false;
+bool itemDetectedB = false;
+
 // Library
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
+#include <ESP32Servo.h>
+
+// object servo
+Servo servoA;
+Servo servoB;
 
 void setup() {
   // pin mode
@@ -29,7 +41,11 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(en, OUTPUT);
   pinMode(trigPin, OUTPUT);  
-	pinMode(echoPin, INPUT);  
+	pinMode(echoPin, INPUT);
+
+  // servo
+  servoA.attach(26);
+  servoB.attach(34); 
 
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
@@ -60,7 +76,34 @@ void loop() {
   distance = (duration*.0343)/2;
   Serial.print("Distance: ");  
 	Serial.println(distance);  
-	delay(100);
+  // Deteksi untuk variabel A (7-10 cm)
+  if (distance < 3) {
+    if (!itemDetectedA) {
+      itemA++;
+      itemDetectedA = true;
+      Blynk.virtualWrite(V2, itemA);
+      servoA.write(90);
+      delay(1000);
+      for (int i = 90; i > 0; i--) {
+        servoA.write(i);
+        delay(20);
+      }
+    }
+  } else {
+    itemDetectedA = false;
+  }
+
+  // Deteksi untuk variabel B (2-6 cm)
+  if (distance > 4 && distance < 8) {
+    if (!itemDetectedB) {
+      itemB++;
+      itemDetectedB = true;
+      Blynk.virtualWrite(V3, itemB);
+    }
+  } else {
+    itemDetectedB = false;
+  }
+  delay(100);
 }
 
 BLYNK_WRITE(V0)
@@ -81,6 +124,19 @@ BLYNK_WRITE(V1)
 {
   int value = param.asInt();
   analogWrite(en,value);
+  Serial.print("V1 Slider value is: ");
+  Serial.println(value);
+}
+
+BLYNK_WRITE(V4)
+{
+  int value = param.asInt();
+  if (value == 1) {
+    itemA = 0;
+    itemB = 0;
+    Blynk.virtualWrite(V2, itemA);
+    Blynk.virtualWrite(V3, itemB);
+  }
   Serial.print("V1 Slider value is: ");
   Serial.println(value);
 }
